db3b94884c3597e1b8521443b4fb6bfc67816859
kevinpicon01 <digitalpruu@gmail.com>
2024-09-27 01:29:34 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,9 +1,9 @@
 const title = "Sandycosturas";
-const email = ""; // Información no proporcionada
+const email = "Sandrajerez2020@hotmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    "¡Hola! Estoy muy interesado en tus productos de " + title + " y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!"
+    "¡Hola! Estoy interesado en conocer más sobre los productos de " + title + " y me gustaría obtener más información. ¡Gracias!"
 );
-const numeroWhatsApp = ""; // Información no proporcionada
+const numeroWhatsApp = "+573175135997"; // Incluido el prefijo +57
 
 const textos = {
     index: {
@@ -13,8 +13,7 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "Sandycosturas se especializa en la confección de cubre lechos y cojines, combinando innovación y calidad en cada pieza. Nuestro lema es 'A cocer y a innovar', lo que refleja nuestro compromiso con la creatividad y la perfección en nuestros productos.",
-        description2: "En Sandycosturas, nos dedicamos a ofrecer productos que no solo cumplen con tus expectativas, sino que también aportan un toque único a tu hogar. Descubre cómo nuestras costuras pueden hacer la diferencia en tu decoración."
+        description1: "Sandycosturas se especializa en la confección de cubre lechos y cojines, combinando innovación y calidad en cada pieza. Con nuestro lema 'A cocer y a innovar', nos comprometemos a ofrecer productos que no solo cumplen con tus expectativas, sino que también aportan un toque único a tu hogar.",
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
@@ -22,14 +21,14 @@ const textos = {
     },
     footer: {
         slogan: {
-            p1: "A cocer y a innovar",
+            p1: "A cocer y a innovar.", // Slogan proporcionado
             p2: ""
         },
         address: "", // Información no proporcionada
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
+        facebook: "https://www.facebook.com/278708455335802", // Enlace específico de Facebook
+        instagram: "https://www.instagram.com", // Link base ya que no se proporcionó uno específico
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
     },
 };
